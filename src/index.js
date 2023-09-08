176ce08865fe8d46add6dedb6e70c17cc0375687
Jonathan Cheng <jcheng461@gmail.com>
2023-09-08 11:01:30 -0700

    set up fetch API and UI logic. WIP: display results properly

--- src/index.js
+++ src/index.js
@@ -1,3 +1,41 @@
 import "bootstrap";
 import "bootstrap/dist/css/bootstrap.min.css";
 import "./css/styles.css";
+import FetchExchangeRateService from "./js/fetchExchangeRateService";
+
+// Business Logic
+function getConversion(amount, currency) {
+  FetchExchangeRateService.fetchConversion(amount, currency).then(function (response) {
+    if (response.ok) {
+      console.log(`Index.js getConversion OK: ${response}`);
+      showTargetCurrencyValue(response);
+    } else {
+      console.log(`Index.js getConversion NOT: ${response}`);
+      showError(response);
+    }
+  });
+}
+
+// UI Logic
+function showTargetCurrencyValue(apiResponse) {
+  document.getElementById("results").innerText = apiResponse.conversion_result;
+}
+
+function showError(request) {
+  document.getElementById("results").innerText = request.status;
+}
+
+function handleFormSubmission(event) {
+  event.preventDefault();
+  const userEnteredAmount = document.getElementById("dollar-amount").value;
+  const userTargetCurrency = "KRW";
+  document.getElementById("dollar-amount").value = null;
+  getConversion(userEnteredAmount, userTargetCurrency);
+}
+
+function formLoader() {
+  let userForm = document.getElementById("user-input-form");
+  userForm.addEventListener("submit", handleFormSubmission);
+}
+
+window.addEventListener("load", formLoader);
--- src/js/fetchExchangeRateService.js
+++ src/js/fetchExchangeRateService.js
@@ -1,5 +1,20 @@
 export default class FetchExchangeRateService {
   static fetchConversion(usdAmount, targetCurrency) {
-    return fetch(`https://v6.exchangerate-api.com/v6/${process.env.API_KEY}/pair/USD/${targetCurrency}/${usdAmount}`);
+    return fetch(`https://v6.exchangerate-api.com/v6/${process.env.API_KEY}/pair/USD/${targetCurrency}/${usdAmount}`)
+      .then(function (response) {
+        console.log(response);
+        if (!response.ok) {
+          const errorMsg = `${response.status} ${response.statusText}`;
+          console.log(`Error: ${response.status} ${response.statusText}`);
+          throw new Error(errorMsg);
+        } else {
+          console.log(`Success: ${response.status} ${response.statusText}`);
+          return response.json();
+        }
+      })
+      .catch(function (error) {
+        console.log(`Error - Catch: ${error}`);
+        return error;
+      });
   }
 }
