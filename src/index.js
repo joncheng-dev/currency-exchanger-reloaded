4472485d9c9b3d686d8fab130cbcbd382903a7d9
Jonathan Cheng <jcheng461@gmail.com>
2023-09-08 15:02:57 -0700

    update messages printed to UI for errors

--- src/index.js
+++ src/index.js
@@ -9,20 +9,31 @@ function getConversionResults(amount, currency) {
   promise.then(function (currencyArray) {
     showTargetCurrencyValue(currencyArray);
   }, function (errorArray) {
-    console.log(`Errors here: ${errorArray}`);
     showError(errorArray);
   });
 }
 
 // UI Logic
 function showTargetCurrencyValue(data) {
-  console.log(data);
-  document.getElementById("results").innerText = `Conversion Result: ${data[0].conversion_result} ${data[0].target_code}`;
+  document.getElementById("results").innerText = `Conversion Result: ${data[1]} USD = ${data[0].conversion_result} ${data[0].target_code}`;
 }
 
 function showError(error) {
-  console.log(error);
-  document.getElementById("results").innerText = `Error. Status: ${error[0].status}. Type: ${error[1]["error-type"]}`;
+  let outputToHtml = "";
+  let customErrorMessage;
+  if (error[0].status === 400) {
+    customErrorMessage = "Request does not follow the accepted structure. Please refer to the three character country codes.";
+  } else if (error[0].status === 404) {
+    customErrorMessage = "Supplied currency code not supported. Please see supported currencies.";
+  } else if (error[0].status === 403) {
+    if (error[1]["error-type"] === "invalid-key") {
+      customErrorMessage = "Please check that the API key in the .env file is inputted correctly and accurately.";
+    } else if (error[1]["error-type"] === "inactive-account") {
+      customErrorMessage = "Please check that your API key is active and functioning. Your email address may not have been confirmed.";
+    }
+  }
+  outputToHtml += `Error. Status: ${error[0].status}. Type: ${error[1]["error-type"]} Message: ${customErrorMessage}`;
+  document.getElementById("results").innerText = outputToHtml;
 }
 
 function handleFormSubmission(event) {
